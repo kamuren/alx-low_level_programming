ab3c821d952eef92c07ba945be30b033835badee
kamuren <kamurenricky254@gmail.com>
2023-02-16 18:11:40 -0800

    output using the puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * gives an output using puts
+ * Return: always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
