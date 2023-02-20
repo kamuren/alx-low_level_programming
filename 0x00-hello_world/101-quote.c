3290b30684e0ed0860c033f25d56a48d6e4d87c1
kamuren <kamurenricky254@gmail.com>
2023-02-19 23:22:31 -0800

    quote -task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * This programms prints the quote 'and the piece
+ * of art is useful\" -Dora Korpar, 2015-10-19
+ *
+ * Return: Always 0. Success.
+ */
+int main(void)
+{
+	fprintf(stderr,
+			"and that piesce of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
