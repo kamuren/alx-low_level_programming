03a6ffa9207652ca160780e9bd073d1e9540a691
kamuren <kamurenricky254@gmail.com>
2023-02-21 14:23:22 -0800

    T 0 - prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main -Entry point
+ *
+ * this functions prints out _putchar 
+ * followed by a new line.
+ *
+ * Return: Always 0. (Success)
+ *
+ */
+int main(void)
+{
+	char output[10] = "_putchar\n";
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(output[i]);
+		i++;
+	}
+	return (0);
+
+}
