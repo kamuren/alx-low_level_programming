dfdee0a29d40541d77e50997e1941f7ff0db9505
kamuren <kamurenricky254@gmail.com>
2023-02-22 04:23:00 -0800

    T 7 FIX

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,12 +14,12 @@ int print_last_digit(int x)
 
 	if (digit < 0)
 	{
-		_putchar(-digit + 0);
+		_putchar(-digit + 48);
 		return (-digit);
 	}
 	else
 	{
-		_putchar(digit + 0);
+		_putchar(digit + 48;
 		return (digit);
 	}
 }
