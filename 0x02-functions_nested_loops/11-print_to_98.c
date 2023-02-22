743e393c8d46d5d8174830670e22a372a0a4245d
kamuren <kamurenricky254@gmail.com>
2023-02-22 08:22:54 -0800

    T 12 -printing range

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Print value of n to 98
+ *
+ * @n: - value to begin from
+ *
+ * Return: nothing to return
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98, i++)
+		{
+			if (i < 98)
+			{
+				putchar(i + 48);
+				putchar(',');
+				putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+
+	else
+
+	{
+		for (i = n; i >= 98, i--)
+		{
+			if (i > 98)
+			{
+				putchar(i + 48);
+				putchar(',');
+				putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+
+}
