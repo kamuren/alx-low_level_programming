4d60057a00bc04e643a91692a9af9f59b26decec
kamuren <kamurenricky254@gmail.com>
2023-02-22 08:32:15 -0800

    T 12 -printing range fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,22 @@ void print_to_98(int n)
 		{
 			if (i < 98)
 			{
-				putchar(i + 48);
+				putchar(i);
+				putchar(',');
+				putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+
+	else if
+
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i > 98)
+			{
+				putchar(i);
 				putchar(',');
 				putchar(' ');
 			}
@@ -28,18 +43,7 @@ void print_to_98(int n)
 	}
 
 	else
+		putchar(n);
 
-	{
-		for (i = n; i >= 98; i--)
-		{
-			if (i > 98)
-			{
-				putchar(i + 48);
-				putchar(',');
-				putchar(' ');
-			}
-			_putchar('\n');
-		}
-	}
 
 }
