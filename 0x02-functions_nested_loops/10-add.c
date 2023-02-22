43e3cce6a69a23be923d8c6fd39b29c815320c37
kamuren <kamurenricky254@gmail.com>
2023-02-22 05:44:16 -0800

    T 11 - addition of integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @x: -parameter
+ * @y: -parameter
+ *
+ * Return: value of added parameter
+ */
+
+int add(int x, int y)
+{
+	result = x + y;
+	return (result);
+}
