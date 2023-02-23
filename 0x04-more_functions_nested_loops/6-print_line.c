b80962ab6b4dfcc16c34638b06093beda5ffd7a8
kamuren <kamurenricky254@gmail.com>
2023-02-23 15:20:09 -0800

    t 6 - line print

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ * @n: shows number of times '_' should be be printed
+ * Return: void
+ */
+
+int print_line(int n)
+{
+	int c;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+		for (c = 0; c <= n; c++)
+			_putchar(95);
+
+	_putchar('\n');
+}
