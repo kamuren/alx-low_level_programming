81e7358593b5a9c4b08c93fc73b9ee73ca2650c3
kamuren <kamurenricky254@gmail.com>
2023-02-23 13:51:11 -0800

    t 3 - multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies the parameters and gives output
+ * @a: first parameter
+ * @b: second parameter
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	_putchar(result + 48);
+
+	return (0);
+}
