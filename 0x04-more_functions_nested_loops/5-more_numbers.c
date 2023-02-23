c37ef8de333e0e08d3073cc4bd45ce00f7ed5c35
kamuren <kamurenricky254@gmail.com>
2023-02-23 14:53:33 -0800

    t 5 -loop through print and fix on main.h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14 ten times
+ * Return: null
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int num;
+
+	i = 0;
+	num = 0;
+	while (i <= 10)
+	{
+		while (num <= 14)
+		{
+			_putchar(num + 48);
+			num++;
+		}
+
+		i++;
+	}
+
+}
