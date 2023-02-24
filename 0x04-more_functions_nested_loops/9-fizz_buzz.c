263f7cccd832812107c42e7527a8097cdea5c362
kamuren <kamurenricky254@gmail.com>
2023-02-23 16:23:45 -0800

    t 9 - fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * fizz_buzz - prints fizz in mult of 3, buzz in those of 5 and 
+ * fizzbuzz in cases where the both apply
+ *
+ * Return: no value
+ */
+
+int fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			_putchar('Fizz');
+			_putchar(' ');
+		}
+		else if (i % 5 == 0)
+		{
+			_putchar('Buzz');
+			_putchar(' ');
+		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			_putchar('FizzBuzz');
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar(i + 48);
+			_putchar(' ');
+		}
+	}
+}
+
+
