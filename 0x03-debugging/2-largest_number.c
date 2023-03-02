8bdfe01997c84058bda766e155917621f7d6ffb8
kamuren <kamurenricky254@gmail.com>
2023-03-02 15:30:42 +0300

    Correction betty

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,21 +5,21 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: largest number
+ * Return: max
  */
 
 int largest_number(int a, int b, int c)
 {
 	int max = a;
-    
+
     	if (b > max)
 
         	max = b;
-    	
+
     	if (c > max)
-	
+
         	max = c;
-    	
+
 
 	return (max);
 }
