597b2b2dd0daa48b00e358ccf63dc4d9810d371c
kamuren <kamurenricky254@gmail.com>
2023-03-02 15:50:07 +0300

    Correction betty

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,14 +12,10 @@ int largest_number(int a, int b, int c)
 {
 	int max = a;
 
-    	if (b > max)
-
-        	max = b;
-
-    	if (c > max)
-
-        	max = c;
-
+	if (b > max)
+		max = b;
+	if (c > max)
+		max = c;
 
 	return (max);
 }
