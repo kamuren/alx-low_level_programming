2e7bca88445ad57a7e5eacafdbfdc1008712784f
kamuren <kamurenricky254@gmail.com>
2023-03-03 05:51:13 +0300

    5. look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letter of
+ * string to uppercase.
+ * @n: parameter for characters
+ * Return: n.
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
